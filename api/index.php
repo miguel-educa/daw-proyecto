fe70d33036c1fff9e9cb02681b264b489cf31418
Miguel <miguelguerrero00@gmail.com>
2025-04-12 11:40:41 +0200

    ADD: API index indicando si el servicio funciona

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hello World";
+require_once __DIR__ . "/env.php";
+require_once __DIR__ . "/" . RESPONSE_PATH;
+require_once __DIR__ . "/" . HTTP_CODE_PATH;
+
+$res = new Response();
+
+$res->showResponseAndExit(HttpCode::OK);
